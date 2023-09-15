c1a0f182a5b108554d310b8d5d88e1d15fcd2fae
TanyaMosk <tanya856227@gmail.com>
2023-09-15 10:52:59 +0200

    Update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,12 @@
 
-
-import storage from 'redux-persist/lib/storage';
+// import storage from 'redux-persist/lib/storage';
 import { contactReducer } from "./contactsSlice";
 import { configureStore } from "@reduxjs/toolkit";
 
-const persistConfig = {
-  key: 'root',
-  storage,
-};
-
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+// };
 
 
 export const store = configureStore({  
